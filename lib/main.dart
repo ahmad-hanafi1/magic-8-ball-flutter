c771455008948b7a2044bd6e7b0842bca666eb49
ahmad-hanafi1 <ahmadhanafi235@gmail.com>
2025-01-16 15:01:37 +0100

    Added the finished app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(magicBall());
+
+class magicBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text(
+            "Ask me anything",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: BallImage(),
       ),
     );
+  }
+}
+
+class BallImage extends StatefulWidget {
+  @override
+  State<BallImage> createState() => _BallImageState();
+}
+
+class _BallImageState extends State<BallImage> {
+  int imageNumber = Random().nextInt(5) + 1;
+
+  void changeImage() {
+    setState(() {
+      imageNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset("images/ball$imageNumber.png"),
+        onPressed: () {
+          print("Hello I'm magic ball!");
+          changeImage();
+        },
+      ),
+    );
+  }
+}
